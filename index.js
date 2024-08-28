043b4fa6897357356431bd6de20331ca208b9a44
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-08-28 08:08:40 -0500

    Project Complete
    Tic Tac Toe app runs/updates front-end and back-end,
    per assignment requirements.

--- index.js
+++ index.js
@@ -1,48 +1,145 @@
 const ticTacToe = (function () {
-    let computer = 0;
-    let markers = ['X', '', '', 'X', '', '', 'X', '', ''];
-    let player = 0;
+    let playerOne = {
+        marker: 'X',
+        name: ''
+    };
 
-    let choiceBoiler = document.createElement('h1');
-    let spaceBoiler = document.createElement('div');
-    choiceBoiler.className = 'choice';
-    spaceBoiler.className = 'player-space';
+    let playerTwo = {
+        marker: 'O',
+        name: ''
+    };
 
-    function generateGameboard () {
+    let game = {
+        currentGame: false,
+        currentPlayer: null,
+        gameWon: false,
+        options: ['', '', '', '', '', '', '', '', ''],
+        possibleWins: [
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 4, 6],
+            [2, 5, 8],
+            [3, 4, 5],
+            [6, 7, 8]
+        ]
+    };
+
+    function generateGameboard() {
+        const spaceBoiler = document.createElement('div');
+        spaceBoiler.className = 'player-space';
         let gameboard = document.createElement('div');
         gameboard.id = 'gameboard';
-        markers.forEach(marker => {
+        //Creates div with h1 (front-end) for each option (back-end):
+        for (let i = 0; i < game.options.length; i++) {
             let currentSpace = spaceBoiler.cloneNode();
-            let currentChoice = choiceBoiler.cloneNode();
-            currentChoice.textContent = marker;
-            currentSpace.appendChild(currentChoice);
+            currentSpace.id = i;
+            currentSpace.textContent = game.options[i];
+            currentSpace.addEventListener('click', () => markSpace(currentSpace.id));
             gameboard.appendChild(currentSpace);
-        });
+        }
 
         return gameboard;
     }
+    
+    function generateMenu() {
+        let menu = document.createElement('div');
+        menu.className = 'menu';
+        const startButton = document.createElement('button');
+        startButton.addEventListener('click', startGame);
+        startButton.id = 'start-button';
+        startButton.textContent = 'Start Game';
+        menu.appendChild(startButton);
 
-    function generateScoreboard() {
-        let scoreboard = document.createElement('div');
-        scoreboard.id = 'scoreboard';
-        let computerScore = document.createElement('div');
-        computerScore.className = 'score';
-        computerScore.id = 'computer-score';
-        computerScore.textContent = `Computer: ${computer}`;
-        let playerScore = document.createElement('div');
-        playerScore.className = 'score';
-        playerScore.id = 'player-score';
-        playerScore.textContent = `Player: ${player}`;
-        scoreboard.append(computerScore, playerScore);
+        return menu;
+    }
 
-        return scoreboard;
+    function markSpace(id) {
+        if (game.currentGame) {
+            if (game.options[id] !== '' || !game.currentGame) {
+                return;
+            }
+            game.options[id] = game.currentPlayer.marker;
+            document.getElementById(id).textContent = game.currentPlayer.marker;
+            winCheck();
+            if (!game.gameWon) {
+                togglePlayer();
+            }
+        }
+    }
+
+    function initializePlayers() {
+        if (Math.floor(Math.random() * 2) === 0) {
+            game.currentPlayer = playerOne;
+        } else {
+            game.currentPlayer = playerTwo;
+        }
+    }
+    
+    function startGame() {
+        game.currentGame = true;
+        initializePlayers();
+        playerOne.name = prompt('What is your name, player one?');
+        playerTwo.name = prompt('What is your name, player two?');
+        console.table(game);
+        [playerOne, playerTwo].forEach(player => {
+            if (player.name === null) {
+                player.name = player.marker;
+            }
+        });
+        updateMenu();
+    }
+    
+    function togglePlayer() {
+        game.currentPlayer === playerOne ? game.currentPlayer = playerTwo : game.currentPlayer = playerOne;
+        document.querySelector('#now-playing').textContent = `${game.currentPlayer.name}'s turn (${game.currentPlayer.marker})`;
+        console.table(game);
+    }
+
+    function updateMenu() {
+        const nowPlaying = document.createElement('h1');
+        nowPlaying.id = 'now-playing';
+        nowPlaying.textContent = `${game.currentPlayer}'s turn`;
+        document.querySelector('#start-button').replaceWith(nowPlaying);
+        nowPlaying.textContent = `${game.currentPlayer.name}'s turn (${game.currentPlayer.marker})`;
+    }
+
+    function winCheck() {
+        for (let i = 0; i < game.possibleWins.length; i++) {
+            const currWinPoss = game.possibleWins[i];
+            const firstChoice = game.options[currWinPoss[0]];
+            const secondChoice = game.options[currWinPoss[1]];
+            const thirdChoice = game.options[currWinPoss[2]];
+
+            if (firstChoice == '' || secondChoice == '' || thirdChoice == '') {
+                continue;
+            }
+            if (firstChoice == secondChoice && secondChoice == thirdChoice) {
+                game.gameWon = true;
+                console.table(game);
+                break;
+            }
+        }
+
+        if (game.gameWon) {
+            game.currentGame = false;
+            game.gameWon = true;
+            alert(`${game.currentPlayer.name} wins!`);
+            location.reload();
+        }
+        else if (!game.options.includes('')) {
+            game.currentGame = false;
+            alert(`It's a draw!`);
+            location.reload();
+        }
     }
 
     return {
         generateGameboard: generateGameboard,
-        generateScoreboard: generateScoreboard
+        generateMenu: generateMenu
     };
 })();
 
 const container = document.querySelector('#container');
-container.append(ticTacToe.generateScoreboard(), ticTacToe.generateGameboard());
+container.append(ticTacToe.generateMenu(), ticTacToe.generateGameboard());
